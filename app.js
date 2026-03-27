885a5f876f8832da3bdf29ebffce24776a7c5f05
PabloPiccolo <161927396+PabloPiccolo@users.noreply.github.com>
2026-03-27 13:36:52 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 const cors = require('cors');
 
 const app = express();
